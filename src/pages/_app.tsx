0c4e83542972b0411679dbe1349d5edf9a0cbde4
Mubariz Ahmed <mebza@live.com>
2025-09-25 13:13:37 +0200

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="absolute w-screen h-screen overflow-hidden">
+    <div className="absolute h-screen w-screen overflow-hidden">
       <Navbar />
-      <div className="absolute w-screen h-screen snap-y overflow-scroll">
-      <Component {...pageProps}/>
+      <div className="absolute h-screen w-screen snap-y overflow-scroll">
+        <Component {...pageProps} />
       </div>
     </div>
   );
